09b89568be391f3d9278c55180c53f3080f844fd
masirg <masirg@ttu.ee>
2019-02-20 20:46:40 +0200

    jquery ui sortable list(drag elements around)

---
+++ List2/resources/js/site.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+
+    $("#addButton").click(function () {
+        $("#list").append("<li><b>" + $("#field1").val() + "  : </b>     " + $("#field2").val() + " <button href='' id='removeButton' class='btn btn-danger btn-sm'>Remove</button></li>")
+   });
+
+    $("#list").sortable({
+        axis: "y"
+    }).disableSelection();
+});
+
+$(document).on("click", "#removeButton", function () {
+    this.parentNode.parentNode.removeChild(this.parentNode);
+});
